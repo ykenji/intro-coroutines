18d42881be7cc6ad90809f667c9a3e0d9c57e4f2
Kenji Yokoyama <Kenji.Yokoyama@sony.com>
2023-02-07 18:29:36 +0900

    Structured concurrency

--- src/tasks/Request5NotCancellable.kt
+++ src/tasks/Request5NotCancellable.kt
@@ -4,6 +4,22 @@ import contributors.*
 import kotlinx.coroutines.*
 import kotlin.coroutines.coroutineContext
 
-suspend fun loadContributorsNotCancellable(service: GitHubService, req: RequestData): List<User> {
-    TODO()
+suspend fun loadContributorsNotCancellable(
+    service: GitHubService,
+    req: RequestData
+): List<User> {
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
+
+    val deferreds: List<Deferred<List<User>>> = repos.map { repo ->
+        GlobalScope.async {
+            log("starting loading for ${repo.name}")
+            service.getRepoContributors(req.org, repo.name)
+                .also { logUsers(repo, it) }
+                .bodyList()
+        }
+    }
+    return deferreds.awaitAll().flatten().aggregate()
 }
